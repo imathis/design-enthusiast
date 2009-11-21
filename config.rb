c549a58cf97ec0e84122823044a1cf279fa7e7e3
B Mathis <brandon@imathis.com>
2009-11-21 15:38:04 -0600

    updated config to use absolute paths for images

--- config.rb
+++ config.rb
@@ -4,5 +4,7 @@ project_type = :stand_alone
 # Set this to the root of your project when deployed:
 http_path = "/"
 css_dir = "site/stylesheets"
-sass_dir = "/stylesheets"
+sass_dir = "stylesheets"
+images_dir = "source/images/"
 #relative_assets = true
+http_images_path = "/images"

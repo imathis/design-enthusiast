70f8e53248459b24a917e4036c364f7f9317935e
B Mathis <brandon@imathis.com>
2009-11-30 15:33:57 -0600

    aliased demos to project/demo

--- _demos/fancy-buttons/config.rb
+++
@@ -1,9 +0,0 @@
-require 'compass-colors'
-require 'fancy-buttons'
-# Require any additional compass plugins here.
-
-project_type = :stand_alone
-css_dir = "stylesheets"
-sass_dir = "src"
-images_dir = "images"
-relative_assets = true

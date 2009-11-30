0abcdd0c030853e01d1b5797a17dfe958ac3e14d
B Mathis <brandon@imathis.com>
2009-11-30 15:00:15 -0600

    added demo for fancy buttons

---
+++ _demos/fancy-buttons/config.rb
@@ -0,0 +1,9 @@
+require 'compass-colors'
+require 'fancy-buttons'
+# Require any additional compass plugins here.
+
+project_type = :stand_alone
+css_dir = "stylesheets"
+sass_dir = "src"
+images_dir = "images"
+relative_assets = true

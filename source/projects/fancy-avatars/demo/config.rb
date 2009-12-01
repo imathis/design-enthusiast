0a71a17a0400df4f9d35d3c66062c933e2294e0d
B Mathis <brandon@imathis.com>
2009-11-30 23:14:07 -0600

    updated demo page for fancy avatars and added two new avatars

--- source/projects/fancy-avatars/demo/config.rb
+++ source/projects/fancy-avatars/demo/config.rb
@@ -1,5 +1,6 @@
 # Require any additional compass plugins here.
 require 'compass-colors'
+require 'fancy-buttons'
 project_type = :stand_alone
 # Set this to the root of your project when deployed:
 http_path = "/"
